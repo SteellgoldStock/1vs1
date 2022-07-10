78373683a5b83fa24e207c257802305dfb1a5ab7
Gaëtan H <steellgold0@gmail.com>
2022-07-10 17:44:08 +0200

    Create CombatCommand.php

---
+++ src/steellgold/combat/commands/CombatCommand.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace steellgold\combat\commands;
+
+use CortexPE\Commando\BaseCommand;
+use dktapps\pmforms\MenuForm;
+use dktapps\pmforms\MenuOption;
+use pocketmine\command\CommandSender;
+use pocketmine\player\Player;
+
+class CombatCommand extends BaseCommand {
+
+	public function onRun(CommandSender $sender, string $aliasUsed, array $args): void {
+		if (!$sender instanceof Player){
+			$sender->sendMessage("§cThis command can only be used in-game.");
+			return;
+		}
+
+		$sender->sendForm($this->getArenasForm());
+	}
+
+	public function getArenasForm() : MenuForm {
+
+		return new MenuForm(
+			"Liste des arènes",
+			"blabla",
+			[
+				new MenuOption("test 1")
+			],
+			function (Player $player, int $selectedOption) : void {
+
+			}
+		);
+	}
+
+	protected function prepare(): void {
+		// TODO: Implement prepare() method.
+	}
+}
