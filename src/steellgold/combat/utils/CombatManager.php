78ab629907d18a955268c04bceb4f1c66725138c
Gaëtan H <steellgold0@gmail.com>
2022-07-10 17:44:20 +0200

    Create CombatManager.php

---
+++ src/steellgold/combat/utils/CombatManager.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace steellgold\combat\utils;
+
+use pocketmine\utils\Config;
+use steellgold\combat\Combat;
+use steellgold\combat\utils\instances\Duel;
+
+class CombatManager {
+
+	/** @var Duel[] */
+	public array $duels;
+
+	public function __construct() {
+		$duels = new Config(Combat::getInstance()->getDataFolder() . "duels.json", Config::JSON);
+		foreach ($duels->getAll() as $duelId => $data) {
+			$this->duels[] = new Duel($duelId);
+		}
+	}
+
+	public function getDuel(string $id): ?Duel {
+		foreach ($this->duels as $duel) {
+			if ($duel->getId() === $id) {
+				return $duel;
+			}
+		}
+		return null;
+	}
+
+	public function duelExist(string $id) : bool {
+		return $this->getDuel($id) !== null;
+	}
+
+	public function getDuels(): array {
+		return $this->duels;
+	}
+}
