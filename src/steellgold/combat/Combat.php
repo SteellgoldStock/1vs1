aba50c43153677e47c6f3d22cf26208f82a19c4e
Gaëtan H <steellgold0@gmail.com>
2022-07-11 18:55:22 +0200

    Update Combat.php

--- src/steellgold/combat/Combat.php
+++ src/steellgold/combat/Combat.php
@@ -32,9 +32,11 @@ class Combat extends PluginBase {
 		if (!file_exists($this->getDataFolder() . "arenas.json")) $this->saveResource("arenas.json");
 		$this->getServer()->getCommandMap()->register("duels", new CombatCommand($this,"arenas","Liste des arènes"));
 		$this->getServer()->getCommandMap()->register("duels", new CombatAdminCommand($this,"arena","Gérer les arènes"));
+
 	protected function onDisable(): void {
 		foreach ($this->manager->getDuels() as $duel) $duel->save();
 	}
+
 	public function getSerializer(): Serialize {
 		return $this->serializer;
 	}
