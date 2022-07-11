f1e494d69639d963298769dda773474e96fe61ab
Gaëtan H <steellgold0@gmail.com>
2022-07-11 18:56:41 +0200

    form listener

--- src/steellgold/combat/commands/CombatCommand.php
+++ src/steellgold/combat/commands/CombatCommand.php
@@ -34,7 +34,20 @@ class CombatCommand extends BaseCommand {
 
 		return new MenuForm(
 			"Liste des arènes",
+			"§l» §rVoici la liste des arènes disponibles:",
 			$buttons,
+			function (Player $player, int $selectedOption) use ($duels) : void {
+				$duel = Combat::getInstance()->getManager()->getDuel($duels[$selectedOption]);
+				if ($duel->getPlayer1() == null) $duel->setPlayer(1, $player);
+				else $duel->setPlayer(2, $player);
+
+				if ($duel->getSlots(true) == 2) {
+					$player->sendMessage("§l» §rVous avez rejoint l'arène §f{$duel->getDisplayName()}");
+					$duel->getPlayer1()->sendMessage("§l» §r{$player->getName()} a rejoint l'arène, début du compte à rebours...");
+					$duel->start();
+				} elseif ($duel->getSlots(true) == 1) {
+					$player->sendMessage("§cL'arène n'est pas complete, attendez qu'un joueur rejoigne");
+				}
 			}
 		);
 	}
