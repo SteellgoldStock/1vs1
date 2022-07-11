1be9e0d2cb79e0bebb605e8e01389bde4665690a
Gaëtan H <steellgold0@gmail.com>
2022-07-11 18:56:51 +0200

    Update PositionCommand.php

--- src/steellgold/combat/commands/subcommands/PositionCommand.php
+++ src/steellgold/combat/commands/subcommands/PositionCommand.php
@@ -6,6 +6,8 @@ use CortexPE\Commando\args\IntegerArgument;
 use CortexPE\Commando\args\RawStringArgument;
 use CortexPE\Commando\BaseSubCommand;
 use pocketmine\command\CommandSender;
+use pocketmine\player\Player;
+use steellgold\combat\Combat;
 
 class PositionCommand extends BaseSubCommand {
 
@@ -15,6 +17,17 @@ class PositionCommand extends BaseSubCommand {
 	}
 
 	public function onRun(CommandSender $sender, string $aliasUsed, array $args): void {
-		var_dump("position");
+		if (!$sender instanceof Player){
+			$sender->sendMessage("§cThis command can only be used in-game.");
+			return;
+		}
+
+		if (!Combat::getInstance()->getManager()->duelExist($args["id"])) {
+			$sender->sendMessage("§cL'identifiant §f{$args["id"]} §cn'est pas attribué à une instance de combat, créer d'abord l'île afin de pouvoir y attribuer un inventaire");
+			return;
+		}
+
+		$duel = Combat::getInstance()->getManager()->getDuel($args["id"]);
+		$duel->setPosition($args["position_id"],$sender->getPosition());
 	}
 }
