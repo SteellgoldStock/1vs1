8746434eee31aed40fded5d78dd4383fe2732274
Gaëtan H <steellgold0@gmail.com>
2022-07-11 18:57:10 +0200

    duel listeners

--- src/steellgold/combat/listeners/DuelListeners.php
+++ src/steellgold/combat/listeners/DuelListeners.php
@@ -3,10 +3,14 @@
 namespace steellgold\combat\listeners;
 
 use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\Server;
+use steellgold\combat\commands\CombatCommand;
 
 class DuelListeners implements Listener {
 
-	public function onDuelCreated(onDuelCreatedEvent $event) {
-		var_dump($event->getDuel()->getId());
+	public function onJoin(PlayerJoinEvent $event) {
+		$player = $event->getPlayer();
+		$player->sendForm(CombatCommand::getArenasForm());
 	}
 }
