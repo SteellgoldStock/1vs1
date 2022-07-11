76ab10aeccdc1a62108644614720f3eb4e7db922
Gaëtan H <steellgold0@gmail.com>
2022-07-11 18:56:48 +0200

    Update InventoryCommand.php

--- src/steellgold/combat/commands/subcommands/InventoryCommand.php
+++ src/steellgold/combat/commands/subcommands/InventoryCommand.php
@@ -5,6 +5,9 @@ namespace steellgold\combat\commands\subcommands;
 use CortexPE\Commando\args\RawStringArgument;
 use CortexPE\Commando\BaseSubCommand;
 use pocketmine\command\CommandSender;
+use pocketmine\player\Player;
+use pocketmine\Server;
+use steellgold\combat\Combat;
 
 class InventoryCommand extends BaseSubCommand {
 
@@ -13,6 +16,19 @@ class InventoryCommand extends BaseSubCommand {
 	}
 
 	public function onRun(CommandSender $sender, string $aliasUsed, array $args): void {
-		var_dump("inventory");
+		if (!$sender instanceof Player){
+			$sender->sendMessage("§cThis command can only be used in-game.");
+			return;
+		}
+
+		if (!Combat::getInstance()->getManager()->duelExist($args["id"])) {
+			$sender->sendMessage("§cL'identifiant §f{$args["id"]} §cn'est pas attribué à une instance de combat, créer d'abord l'île afin de pouvoir y attribuer un inventaire");
+			return;
+		}
+
+		$duel = Combat::getInstance()->getManager()->getDuel($args["id"]);
+		$duel->setInventory($sender->getInventory()->getContents());
+		$duel->setArmor($sender->getArmorInventory()->getContents());
+		$duel->setOffhand($sender->getOffhandInventory()->getContents());
 	}
 }
