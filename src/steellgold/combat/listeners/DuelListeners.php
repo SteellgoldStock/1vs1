d210ab75752fb27ce73d0e0495b51f05771d3d81
Gaëtan H <steellgold0@gmail.com>
2022-07-14 17:54:01 +0200

    Update DuelListeners.php

--- src/steellgold/combat/listeners/DuelListeners.php
+++ src/steellgold/combat/listeners/DuelListeners.php
@@ -2,10 +2,12 @@
 
 namespace steellgold\combat\listeners;
 
+use pocketmine\event\block\BlockPlaceEvent;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\event\entity\EntityDamageEvent;
 use pocketmine\event\Listener;
-use pocketmine\event\player\PlayerDeathEvent;
 use pocketmine\event\player\PlayerJoinEvent;
-use pocketmine\Server;
+use pocketmine\player\Player;
 use steellgold\combat\Combat;
 use steellgold\combat\commands\CombatCommand;
 use steellgold\combat\utils\CombatManager;
@@ -18,12 +20,31 @@ class DuelListeners implements Listener {
 		$player->sendForm(CombatCommand::getArenasForm());
 	}
 
-	public function onDeath(PlayerDeathEvent$event) {
+	public function onFight(EntityDamageByEntityEvent $ev) {
+		$player = $ev->getEntity();
+		if ($player instanceof Player) {
+			if ($player->getHealth() - $ev->getFinalDamage() <= 0) {
+				if ($ev->getCause() == EntityDamageEvent::CAUSE_ENTITY_ATTACK) {
+					$killer = $player->getLastDamageCause()->getDamager();
+					if ($killer instanceof Player) {
+						if (key_exists($player->getName(), CombatManager::$players)) {
+							$duel = Combat::getInstance()->getManager()->getDuel(CombatManager::$players[$player->getName()]);
+							if ($duel instanceof Duel) {
+								$duel->end($killer, $player);
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	public function onBlockPlaced(BlockPlaceEvent $event) {
 		$player = $event->getPlayer();
 		if (key_exists($player->getName(), CombatManager::$players)) {
-			$duel = Combat::getInstance()->getManager()->getDuel($player);
+			$duel = Combat::getInstance()->getManager()->getDuel(CombatManager::$players[$player->getName()]);
 			if ($duel instanceof Duel) {
-				$duel->end();
+				$duel->addBlock($event->getBlock()->getPosition());
 			}
 		}
 	}
